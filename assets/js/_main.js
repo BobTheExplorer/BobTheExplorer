06fd46272a9b02c1cb99fad342b0e39a6fd0ac52
BillTheExplorer.com <74156136+BobTheExplorer@users.noreply.github.com>
2021-09-26 12:46:57 -0400

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -6,23 +6,6 @@ $(document).ready(function() {
   // FitVids init
   $("#main").fitVids();
   
-   var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
 
   // Sticky sidebar
   var stickySideBar = function() {
@@ -153,4 +136,20 @@ function showDivs(n) {
   });
 });
 
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+});
