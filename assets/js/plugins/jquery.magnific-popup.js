24c7155226cf082814761ed160140a372cd91bb3
BillTheExplorer.com <74156136+BobTheExplorer@users.noreply.github.com>
2024-05-17 19:55:58 -0400

    Update jquery.magnific-popup.js

--- assets/js/plugins/jquery.magnific-popup.js
+++ assets/js/plugins/jquery.magnific-popup.js
@@ -1137,6 +1137,9 @@
                 '<div class="mfp-bottom-bar">'+
                   '<div class="mfp-title"></div>'+
                   '<div class="mfp-counter"></div>'+
+                  '<div class="svg-test">
+                  <img src="equilateral.svg" height="87" width="100" />
+                  </div>'+
                 '</div>'+
               '</figcaption>'+
             '</figure>'+
